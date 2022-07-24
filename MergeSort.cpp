8bdb37aaa1d23e55399efb02cd215afddc2cc7c3
Hartej Singh <86732914+HartejSingh2001@users.noreply.github.com>
2022-07-24 11:25:51 +0530

    Update MergeSort.cpp

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,58 +1,76 @@
-class Solution
+// Apni kaksha -- best
+//Merge sort
+#include <iostream>
+using namespace std;
+
+void merge(int arr[],int l,int mid, int r)
 {
-    public:
-    void merge(int arr[], int l, int m, int r)// m=mid
+    int n1,n2,i=0,j=0;
+    n1=mid-l+1;
+    n2=r-mid;
+    int a[n1],b[n2];
+    for(i=0;i<n1;i++)
     {
-         int i=l,j=m+1,k=l;
-         int b[r+1];
-         while(i<=m && j<=r)
-         {
-             if(arr[i]<arr[j])
-             {
-                 b[k]=arr[i];
-                 k++;
-                 i++;
-             }
-            else
-            {
-                b[k]=arr[j];
-                k++;
-                j++;
-            }
-         }
-         if(i>m)
-         {
-             while(j<=r)
-             {
-                 b[k]=arr[j];
-                 j++;
-                 k++;
-             }
-         }
-         else
-         {
-             while(i<=m)
-             {
-                 b[k]=arr[i];
-                 i++;
-                 k++;
-             }
-         }
-         
-         for(k=l;k<=r;k++)
-         {
-             arr[k]=b[k];
-         }
+        a[i]=arr[l+i];
     }
-    public:
-    void mergeSort(int arr[], int l, int r)
+    for(i=0;i<n2;i++)
     {
-       if(l<r)
-       {
-           int m=(l+r)/2;
-           mergeSort(arr,l,m);
-           mergeSort(arr,m+1,r);
-           merge(arr,l,m,r);
-       }
+        b[i]=arr[mid+1+i];
     }
-};
+    int k=l;
+    i=0;
+    while(i<n1 && j<n2)
+    {
+        if(a[i]<b[j])
+        {
+            arr[k]=a[i];
+            i++;k++;
+        }
+        else
+        {
+            arr[k]=b[j];
+            j++;k++;
+        }
+    }
+    while(i<n1)
+    {
+        arr[k]=a[i];
+        i++;k++;
+    }
+    while(j<n2)
+    {
+        arr[k]=b[j];
+        j++;k++;
+    }
+}
+void mergesort(int arr[],int l,int r)
+{
+    if(l<r)
+    {
+        int mid=(l+r)/2;
+        mergesort(arr,l,mid);
+        mergesort(arr,mid+1,r);
+        
+        merge(arr,l,mid,r);
+    }
+}
+int main()
+{
+    int n,i;
+    cout<<"Enter n::";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the array elements:: ";
+    for(i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int l=0,r=n-1;
+    mergesort(arr,l,r);
+    cout<<"The sorted elements are::";
+    for(i=0;i<n;i++)
+    {
+        cout<<arr[i];
+    }
+    return 0;
+}
